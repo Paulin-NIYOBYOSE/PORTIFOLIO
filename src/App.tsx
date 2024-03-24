9869a2e48cf3132709c90a3a08e0c713df84e1d9
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-03-24 10:49:49 +0200

    Also added theme changing to the landon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import react from "react";
-import Nav from "./components/Nav";
+import Nav from "./components/Landon";
 import Landon from "./components/Landon";
-import Test from "./components/Test";
 function App() {
   return (
     <>
-      <Nav />
       <Landon />
-      <Test />
     </>
   );
 }
